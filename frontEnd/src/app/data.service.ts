8ed6a9b5ffe4b94a55247313a38c63c48dd1b68f
Francisco Dimmer <ftdimmer@gmail.com>
2024-10-17 21:30:23 -0300

    navbar

--- frontEnd/src/app/data.service.ts
+++ frontEnd/src/app/data.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root', // Esto permite que el servicio esté disponible en toda la aplicación
 })
 export class DataService {
-  private apiUrl = 'https://api.example.com/data'; // Reemplaza con tu URL de API
+  private apiUrl = 'http://localhost:5000/api/edificio'; // Reemplaza con tu URL de API
 
   constructor(private http: HttpClient) {}
 
